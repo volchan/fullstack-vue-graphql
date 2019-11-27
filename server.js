a3472201f3b75571b681f9ba4ab319935fe8782a
stephane de la torre <s.dlt@hotmail.fr>
2019-11-27 18:30:38 +0100

    Section 9 completed

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ const typeDefs = gql`
   type Query {
     getTodos: [Todo]
   }
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;
+    }
   }
 };
 
