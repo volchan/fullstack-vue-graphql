636cf2874c1e8f11612ae0c2eaa075b6da72bf95
stephane de la torre <s.dlt@hotmail.fr>
2019-11-28 18:39:44 +0100

    Section 12 completed

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  avatar: {
+    type: String
+  },
+  joinDate: {
+    type: Date,
+    default: Date.now
+  },
+  favorites: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Post",
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "variables.env" });
 
+const User = require("./models/User");
+const Post = require("./models/Post");
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
@@ -25,7 +29,11 @@ const typeDefs = gql`
 `;
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
