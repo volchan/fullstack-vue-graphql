80748041f38a4949e2e0db1980ee8ad34055d3a0
stephane de la torre <s.dlt@hotmail.fr>
2019-11-28 19:15:32 +0100

    Section 15 completed

--- resolvers.js
+++ resolvers.js
@@ -3,6 +3,20 @@ module.exports = {
     getPosts: () => []
   },
   Mutation: {
+    addPost: async (
+      _,
+      { title, imageUrl, categories, description, creatorId },
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        imageUrl,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
+    },
     signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
       if (user) {
