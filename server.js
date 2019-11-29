8e0d74152350351c3af6a7310d9d39cd71edc8c4
stephane de la torre <s.dlt@hotmail.fr>
2019-11-29 19:03:51 +0100

    Section 17 completed

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+// Import typeDefs
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 
+// Import models and resolvers
 const User = require("./models/User");
 const Post = require("./models/Post");
 const resolvers = require("./resolvers");
 
+// Import environment variables
 require("dotenv").config({ path: "variables.env" });
 
+// Connect to MongoDB Atlas
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -21,6 +25,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+// Create Apollo/GraphQL Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
