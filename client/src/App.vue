c3980011cce0266320e0faed7ad74b48b2a57008
stephane de la torre <s.dlt@hotmail.fr>
2019-11-30 10:36:03 +0100

    Section 23 completed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app style="background: #E3E3EE">
     <!-- Horizontal Navbar -->
     <v-app-bar
       fixed
@@ -7,19 +7,19 @@
       dark
       app
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-icon></v-icon>
+      <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+
       <v-toolbar-title class="hidden-xs-only">
         <router-link
           to="/"
           tag="span"
-          style="cursor: pointer"
+          style="cursor: pointer;"
         >
           VueShare
         </router-link>
       </v-toolbar-title>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
       <v-text-field
         prepend-icon="mdi-magnify"
@@ -29,11 +29,10 @@
         hide-details
       ></v-text-field>
 
-      <v-spacer class="hidden-xs-only"></v-spacer>
+      <v-spacer class="hidden-xs-only" />
 
       <v-toolbar-items class="hidden-xs-only">
         <v-btn
-          flat
           small
           depressed
           color="primary"
@@ -50,6 +49,48 @@
       </v-toolbar-items>
     </v-app-bar>
 
+    <v-navigation-drawer
+      app
+      temporary
+      fixed
+      v-model="sideNav"
+      light
+    >
+      <v-toolbar
+        color="accent"
+        dark
+        flat
+      >
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+
+        <router-link
+          to="/"
+          tag="span"
+          style="cursor: pointer"
+        >
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+
+      <v-divider />
+
+      <v-list dense>
+        <v-list-item
+          ripple
+          v-for="item in sideNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-item-icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <!-- App Content -->
     <main>
       <v-container class="mt-12">
@@ -62,13 +103,30 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
-        { icon: "mdi-chat", title: "Posts", link: "/posts" },
+        { icon: "mdi-message-text", title: "Posts", link: "/posts" },
         { icon: "mdi-lock-open", title: "Sign In", link: "/login" },
         { icon: "mdi-pencil", title: "Sign Up", link: "/signup" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "mdi-message-text", title: "Posts", link: "/posts" },
+        { icon: "mdi-lock-open", title: "Sign In", link: "/login" },
+        { icon: "mdi-pencil", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };
