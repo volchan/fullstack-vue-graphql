7a059646584a2d8830bc63e4ed46f3e6458bf1c7
stephane de la torre <s.dlt@hotmail.fr>
2019-12-07 15:02:11 +0100

    Section 33 completed

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -30,6 +32,29 @@ const UserSchema = new mongoose.Schema({
     ref: "Post",
     required: true
   }
+});
+
+UserSchema.pre("save", function(next) {
+  this.avatar = `https://gravatar.com/avatar/${md5(this.email)}?d=identicon`;
+  next();
+});
+
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
